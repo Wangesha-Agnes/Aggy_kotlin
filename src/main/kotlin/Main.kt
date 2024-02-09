7b72e47890cbaffeafe5fbfcdf8d872f325e3124
Wangesha-Agnes <wangeshaagnes@gmail.com>
2024-02-09 13:18:25 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(){
+    var studentName = "Agnes Wangesha"
+    studentName = "Agnes Wangesha"
+    println(studentName)
+    var age = "22"
+    age = "22"
+    println(age)
+    var phoneNum = "0797792275"
+    phoneNum = "0797792275"
+    println(phoneNum)
+    var weight = "50"
+    println(weight)
+    var nationality = "Kenyan"
+    println(nationality)
+
+
+
+
+}
